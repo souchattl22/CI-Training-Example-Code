c012dcc432fba2435e2f049a66af28423c47e16e
souchattl22 <soumilchatterjee22@gmail.com>
2016-05-05 12:16:37 +0530

    build.gradle pushed

---
+++ BankAppOnGit/src/main/java/com/cg/demo/service/BankClient.java
@@ -0,0 +1,10 @@
+package com.cg.demo.service;
+
+public class BankClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Banker");
+	}
+
+}
